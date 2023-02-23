e641bd1559f100b14c9e7185e5402f39a18cdbd5
Santhosh <santhoshkalish333@gmail.com>
2023-02-24 02:28:32 +0530

    local storage needed

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-
+// selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-todoButton.addEventListener('click', AddTodo);
 
+// Event Listeners
+todoButton.addEventListener('click', AddTodo);
+todoList.addEventListener('click', TodoDeleteChange)
+
+
+// Functions
 function AddTodo(event) {
 
     event.preventDefault();
-    const Todo = document.createElement('div');
-    Todo.classList.add('todo');
 
-    const todoText = document.createElement('li');
-    todoText.innerText = 'Hello';
-    Todo.append(todoText);
-    // Add Check Button
-    const todoChecked = document.createElement('button');
-    todoChecked.innerHTML = '<i class="fas fa-check"></i>'; 
-    todoChecked.classList.add('todo-checked');
-    Todo.appendChild(todoChecked);
-    // Add Delete Button
-    const todoDelete = document.createElement('button');
-    todoDelete.innerHTML = '<i class="fas fa-trash"></i>';
-    todoDelete.classList.add('todo-delete');
-    Todo.appendChild(todoDelete);
+    if (todoInput.value !== ''){
 
-    todoList.appendChild(Todo)
+        const Todo = document.createElement('div');
+        Todo.classList.add('todo');
+    
+        const todoText = document.createElement('li');
+        todoText.innerText = todoInput.value;
+        Todo.append(todoText);
+        // Add Check Button
+        const todoChecked = document.createElement('button');
+        todoChecked.innerHTML = '<i class="fas fa-check"></i>'; 
+        todoChecked.classList.add('todo-checked');
+        Todo.appendChild(todoChecked);
+        // Add Delete Button
+        const todoDelete = document.createElement('button');
+        todoDelete.innerHTML = '<i class="fas fa-trash"></i>';
+        todoDelete.classList.add('todo-delete');
+        Todo.appendChild(todoDelete);
+    
+        todoList.appendChild(Todo);
+
+        document.querySelector('.todo-add').reset();
+    }
+
 }
+
+function TodoDeleteChange(e) {
+    const item = e.target;
+    //Delete Todo 
+    if (item.classList[0] === 'todo-delete'){
+        todo = item.parentElement; 
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', () => {
+            todo.remove();
+        })
+    }
+    //Check Mark
+    else if (item.classList[0] == 'todo-checked') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
+} 
