f4544eea9ecd9147c9749aa24f4bbe2e66b36af0
Santhosh <santhoshkalish333@gmail.com>
2023-02-24 00:54:21 +0530

    half

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 
-const todoInput = document.querySelector('todo-input')
-const todoButton = document.querySelector('todo-button')
-const todoList = document.querySelector('todo-list')
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
-todoButton.addEventListener('click') = AddTodo
+todoButton.addEventListener('click', AddTodo);
 
 function AddTodo(event) {
 
-    const Todo = document.createElement('div')
+    event.preventDefault();
+    const Todo = document.createElement('div');
 
-    const todoText = document.createElement('li')
-    todoText.innerText = 'Hello'
-    Todo.append(todoText)
+    const todoText = document.createElement('li');
+    todoText.innerText = 'Hello';
+    Todo.append(todoText);
 
-    const todoChecked = document.createElement('button')
-    todoChecked.innerHTML = 
-    checked.classList.add('todo-checked')
-    Todo.appendChild(todoChecked)
+    const todoChecked = document.createElement('button');
+    todoChecked.innerHTML = 'None'; 
+    todoChecked.classList.add('todo-checked');
+    Todo.appendChild(todoChecked);
 
-    const todoDelete = document.createElement('button')
-    todoDelete.innerHTML = 
-    checked.classList.add('todo-delete')
-    Todo.appendChild(todoDelete)
+    const todoDelete = document.createElement('button');
+    todoDelete.innerHTML = 'None';
+    todoDelete.classList.add('todo-delete');
+    Todo.appendChild(todoDelete);
 
+    todoList.appendChild(Todo)
 }
